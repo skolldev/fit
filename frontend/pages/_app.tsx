88127ed1a05444c7143151aafc819a5676540240
Alexander May <alexander.may@kratzer-automation.com>
2020-01-16 17:20:30 +0100

    example how to pass state around using context

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,14 +1,30 @@
 import App from "next/app";
 import React from "react";
 import { exerciseLibrary } from "../mock-data/exercise-library";
-import { IExerciseLibrary } from "../models/exercise-library.interface";
 import ExerciseLibraryContext from "../components/ExerciseLibraryContext";
 
 class DefaultApp extends App {
+  state = {
+    library: exerciseLibrary
+  };
+
+  counter = 4;
+  public addExercise(): void {
+    const lib = { ...this.state.library };
+    const id = this.counter.toString();
+    lib[id] = {
+      displayName: `Exercise ${id}`,
+      equipment: "Barbell",
+      image: "https://webkit.org/demos/srcset/image-src.png"
+    };
+    this.setState({ library: lib });
+    this.counter++;
+  }
+
   public render(): JSX.Element {
     const { Component, pageProps } = this.props;
     return (
-      <ExerciseLibraryContext.Provider value={exerciseLibrary}>
+      <ExerciseLibraryContext.Provider value={this.state.library}>
         <Component {...pageProps} />
       </ExerciseLibraryContext.Provider>
     );
